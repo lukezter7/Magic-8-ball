d050147d28f4ea00247d7bbf051389c658376745
lukezter7 <144057186+lukezter7@users.noreply.github.com>
2023-09-10 22:38:49 -0400

    Create eight.js

---
+++ eight.js
@@ -0,0 +1,52 @@
+var userName = '';
+
+if (userName == '') {
+console.log('Hello!');
+
+}
+
+else if (console.log(`Hello, ${userName}!`));
+
+var userQuestion = 'whats up?';
+
+console.log(userQuestion);
+
+var randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
+
+var eightBall = '';
+
+
+if (randomNumber == 0) {
+console.log('It is certain');
+}
+
+if (randomNumber == 1) {
+    console.log('It is decidedly so');
+    }
+
+    if (randomNumber == 2) {
+        console.log('Reply hazy try again');
+        }
+
+        if (randomNumber == 3) {
+            console.log('Cannot predict now');
+            }
+
+
+            if (randomNumber == 4) {
+                console.log('Do not count on it');
+                }
+
+                if (randomNumber == 5) {
+                    console.log('My sources say no');
+                    }
+
+                    if (randomNumber == 6) {
+                        console.log('Outlook not so good');
+                        }
+
+                        if (randomNumber == 7) {
+                            console.log('Signs point to yes');
+                            }
